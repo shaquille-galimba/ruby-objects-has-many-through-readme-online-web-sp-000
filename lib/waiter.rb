fba43767b4dcf014d184be6da212a4782753f85b
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-26 09:52:12 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -25,4 +25,9 @@ class Waiter
     end
     best_tipped_meal.customer
   end
+
+  def most_freq_customer
+    most_freq_customer = meals.max_by {|meal| meal.customers.length}
+    most_freq_customer
+  end
 end
