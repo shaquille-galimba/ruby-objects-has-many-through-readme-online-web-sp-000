ee68e156143009497ff481e7cdd6d35988cc6cf4
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-02-26 08:56:59 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,13 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
+  @@all = []
 
+  def initialize(waiter, customer, total, tip=0)
+    @waiter, @customer, @total, @tip = waiter, customer, total, tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
